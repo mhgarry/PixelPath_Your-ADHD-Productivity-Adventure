2d9a9fbfd954978a28befe0a1a80b0a17413367c
Matthew Garry <mhgarry92@gmail.com>
2024-06-09 22:00:12 -0400

    routes work for login and auth, clean up

--- backend/config/database.js
+++
@@ -1,6 +0,0 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres' });
-
-module.exports = sequelize;
--- backend/controllers/avatarController.js
+++ backend/controllers/avatarController.js
@@ -1,30 +1,30 @@
-// controllers/avatarController.js
-const { Avatar } = require('../models/Avatar');
+// // controllers/avatarController.js
+// const { Avatar } = require('../models/Avatar');
 
-exports.save = async (req, res) => {
-  try {
-    const { skintone, hair, clothes, eyes } = req.body;
-    const userId = req.user.id; // Assuming req.user contains the authenticated user's data
+// exports.save = async (req, res) => {
+//   try {
+//     const { skintone, hair, clothes, eyes } = req.body;
+//     const userId = req.user.id; // Assuming req.user contains the authenticated user's data
 
-    let avatar = await Avatar.findOne({
-      where: {
-        userId,
-      },
-    });
+//     let avatar = await Avatar.findOne({
+//       where: {
+//         userId,
+//       },
+//     });
 
-    if (avatar) {
-      avatar.customizationData = { skintone, hair, clothes, eyes };
-      await avatar.save();
-    } else {
-      avatar = await Avatar.create({
-        customizationData: { skintone, hair, clothes, eyes },
-        userId,
-      });
-    }
+//     if (avatar) {
+//       avatar.customizationData = { skintone, hair, clothes, eyes };
+//       await avatar.save();
+//     } else {
+//       avatar = await Avatar.create({
+//         customizationData: { skintone, hair, clothes, eyes },
+//         userId,
+//       });
+//     }
 
-    res.status(200).json({ avatar });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-};
+//     res.status(200).json({ avatar });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// };
