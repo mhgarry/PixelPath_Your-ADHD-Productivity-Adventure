60797b33c9fdd93ea69915d1fcb5734335062096
Matthew Garry <mhgarry92@gmail.com>
2024-06-09 21:36:21 -0400

    make auth middleware

---
+++ backend/middleware/authenticate.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const authenticate = (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+  if (!token) {
+    return res.status(401).json({ error: 'Authentication failed' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Authentication failed' });
+  }
+};
+
+module.exports = authenticate;
